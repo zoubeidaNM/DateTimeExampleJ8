d8cb26b986e7473db75a0be07b2817c737434807
zoubeidaNM <zoumenz@yahoo.com>
2017-10-26 14:21:41 -0400

    modified to the requirement of the assignment

--- src/me/aoa4eva/Main.java
+++ src/me/aoa4eva/Main.java
@@ -22,16 +22,21 @@ public class Main {
         DateTimeFormatter shortMonthFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
         DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
+        DateTimeFormatter shortMonthFormat2 = DateTimeFormatter.ofPattern("dd, MMM yyyy");
+        DateTimeFormatter shortMonthFormat3 = DateTimeFormatter.ofPattern("MMM, dd yyyy");
+        DateTimeFormatter dTF2 = DateTimeFormatter.ofPattern("dd, MM yyyy");
+
+
         //Time formatter (time only)
         DateTimeFormatter hr24 = DateTimeFormatter.ofPattern("kk:m");
 
         //Output today's date in formats that have been preset
-        System.out.println("The current date is: "+rightNow.format(longFormat));
-        System.out.println("This is the current date and time unformatted: "+rightNow);
+       // System.out.println("The current date is: "+rightNow.format(longFormat));
+       // System.out.println("This is the current date and time unformatted: "+rightNow);
 
         //Output the current time in formats that have been preset
-        System.out.println("This is the current system time: "+ LocalTime.now());
-        System.out.println("This is the current system time (24 h format): "+LocalTime.now().format(hr24));
+        //System.out.println("This is the current system time: "+ LocalTime.now());
+        //System.out.println("This is the current system time (24 h format): "+LocalTime.now().format(hr24));
 
 
         do{
@@ -39,11 +44,18 @@ public class Main {
             try{
                 userDate = LocalDate.parse(keyboard.nextLine(),dTF);
 
+                if(userDate.isAfter(LocalDate.now())) {
+                    System.out.println("The date must be in the past");
+                    userDate = null;
+                }else{
+
+                    System.out.println("The date entered format  day, Month (3 letters) and year (4 digit) : " + userDate.format(shortMonthFormat2));
+                    System.out.println("The date entered format  Month (3 letters), day  and year (4 digit): " + userDate.format(shortMonthFormat3));
+                    System.out.println("The date entered format  day, month (2 digits) and year format     : " + userDate.format(dTF2));
+                }
+
             }catch(Exception e)
             {
-                if(userDate.isAfter(LocalDate.now())&&userDate!=null)
-                    System.out.println("The date must be in the past");
-
                    System.out.println("Unable to convert the string you entered to date. Please try again!");
 
             }
@@ -51,7 +63,7 @@ public class Main {
         }while(userDate==null);
 
         //Display the date entered
-       System.out.println(userDate.format(shortMonthFormat));
+       //System.out.println(userDate.format(shortMonthFormat));
 
     }
 }
