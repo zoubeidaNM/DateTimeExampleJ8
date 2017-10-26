92a42fcd2a6dabe99f2c824fc1b7ad114562af40
aoa4eva <afua.ankomah@gmail.com>
2017-10-26 12:27:33 +0000

    Introduction to Java 8 Dates

---
+++ src/me/aoa4eva/Main.java
@@ -0,0 +1,46 @@
+package me.aoa4eva;
+
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        //Get the current time
+        LocalDateTime rightNow= LocalDateTime.now();
+        //Date from the user
+        LocalDate userDate=null;
+
+        //Set up formatters so that you can use them later
+        DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter shortMonthFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+
+        DateTimeFormatter hr24 = DateTimeFormatter.ofPattern("kk:m");
+        System.out.println("The current date is: "+rightNow.format(longFormat));
+        System.out.println("This is the current date and time unformatted: "+rightNow);
+        System.out.println("This is the current system time: "+ LocalTime.now());
+        System.out.println("This is the current system time (24 h format): "+LocalTime.now().format(hr24));
+
+
+        do{
+            System.out.println("Enter a date in the past(dd/mm/yyyy)");
+            try{
+                userDate = LocalDate.parse(keyboard.nextLine(),dTF);
+            }catch(Exception e)
+            {
+                System.out.println("Unable to convert the string you entered to date. Please try again!");
+            }
+        }while(userDate==null);
+
+        //Display the date entered
+        System.out.println(userDate.format(shortMonthFormat));
+
+    }
+}
